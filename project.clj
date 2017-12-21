54aade93af7c76ea6abc5a0f92b3d684f9e6d930
Calvin Sauer <calvin.j.sauer@gmail.com>
2017-12-21 18:55:33 -0500

    Snapshot version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tightrope/lein-template "0.1.0"
+(defproject tightrope/lein-template "0.2.0-SNAPSHOT"
   :description "An opinionated, real-time application template for Clojure(Script)"
   :url "https://github.com/cjsauer/tightrope"
   :license {:name "Eclipse Public License"
